7b9ad6f796cad4545c39f4f6b3740fd2a259ecd2
BalanAndreiBogdan <62728569+BalanAndreiBogdan@users.noreply.github.com>
2022-03-22 22:28:51 +0200

    [Updates]

--- src/SinglePlayer.js
+++ src/SinglePlayer.js
@@ -1,10 +1,39 @@
 import React from "react"
 import './App.css';
+import './SinglePlayer.css';
 
 function SinglePlayer() {
+
+  const BoardCreation = () =>{
+    const rowViews = []
+    for (let r = 0; r < 6; r++) {
+      const columnViews = []
+      for (let c = 0; c < 7; c++) {
+        columnViews.push(
+          <div className="square">
+            <div className="circle"></div>
+          </div>
+        )
+      }
+      rowViews.push(
+        <div className="column">{columnViews}</div>
+      );
+    }
+
+    return(
+      <div className="round-border">
+        <div className="board">
+          {rowViews}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="SinglePlayer">
-     <h1>Single player page</h1>
+      <div>
+        {BoardCreation()}
+      </div>
     </div>
   );
 }
